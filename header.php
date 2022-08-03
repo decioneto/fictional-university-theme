9a89385d50130b0f65395f11473d0ec320ccda47
decioneetoW <decio.neeto@gmail.com>
2022-08-03 17:19:39 -0300

    Páginas, blog e archives

--- header.php
+++ header.php
@@ -1,24 +1,38 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
   <head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initia-scale='">
     <?php wp_head(); ?>
   </head>
-  <body>
+  <body <?php body_class(); ?> >
     <header class="site-header">
       <div class="container">
         <h1 class="school-logo-text float-left">
-          <a href="#"><strong>Fictional</strong> University</a>
+          <a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
         </h1>
-        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+        <span class="js-search-trigger site-header__search-trigger">
+          <i class="fa fa-search" aria-hidden="true"></i>
+        </span>
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
+            <!-- <?php
+              wp_nav_menu(array(
+                'theme_location' => 'headerMenuLocation'
+              ));
+            ?> -->
+
             <ul>
-              <li><a href="#">About Us</a></li>
+              <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 16) echo 'class="current-menu-item"' ?> >
+                <a href="<?php echo site_url('/about-us') ?>">About Us</a>
+              </li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
-              <li><a href="#">Blog</a></li>
+              <li <?php if(is_page('blog')) echo 'class="current-menu-item"' ?> >
+                <a href="<?php echo site_url('/blog') ?>">Blog</a>
+              </li>
             </ul>
           </nav>
           <div class="site-header__util">
